cedfacb14b08ab6b680a28b98784e41cbf8ad889
Jugal-Shrestha <jugalnepali@gmail.com>
2022-10-11 20:47:10 +0545

    Optimized A lot

--- Scripts/parallax.js
+++ Scripts/parallax.js
@@ -2,9 +2,8 @@
 const screen= document.getElementById('standingProud')
 const screen2= document.getElementById('allWel3')
 const screen3= document.getElementById('projectsIntroId')
-const aboutH= document.getElementById('headerForAboutSectionId')
-const aboutI= document.getElementById('introForAboutSecId')
 const projectH= document.getElementById('headerForProjectId')
+
 
 window.addEventListener('scroll',function(){
     var value = window.scrollY;
@@ -12,8 +11,6 @@ window.addEventListener('scroll',function(){
     screen.style.top = value *0.5+ 'px';
     screen2.style.top =value *0.7+'px';
     screen3.style.left= value *0.3+'px';
-    aboutH.style.right= value *0.3+'px';
-    aboutI.style.bottom= value *0.2+'px';
     projectH.style.top= value *0.5+ 'px';
 } )
 
