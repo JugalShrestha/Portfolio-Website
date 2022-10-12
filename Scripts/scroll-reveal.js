8d26ed462f5407eff71121220908f96d64dc5183
Jugal-Shrestha <jugalnepali@gmail.com>
2022-10-13 00:10:29 +0545

    Redesigned Everything

--- Scripts/scroll-reveal.js
+++ Scripts/scroll-reveal.js
@@ -12,6 +12,5 @@ ScrollReveal().reveal('#headerForProjectId',{delay:1});
 ScrollReveal().reveal('#headerForAboutSectionId',{origin:'right'});
 ScrollReveal().reveal('#introForAboutSecId',{delay: 400});
 ScrollReveal().reveal('#aboutAllBoxesId',{origin:'left', delay:400});
-ScrollReveal().reveal('#standingProud',{delay:300});
 ScrollReveal().reveal('#Contacts-Header',{delay: 300,origin:'left'});
-ScrollReveal().reveal('#Email-Form-Id',{delay:500});
+
