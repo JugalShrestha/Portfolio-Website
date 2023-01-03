10ee5c1ddcf16e9014b8f7d61c5dd2a0db2c7d97
Jugal Hero <jugalnepali@gmail.com>
2023-01-03 21:35:31 +0545

    added a new GUI Project section left to touch still

--- Scripts/loader.js
+++ Scripts/loader.js
@@ -1,7 +1,7 @@
-const loadingScreen= document.getElementById('loading-screen-id')
-const circle= document.getElementById('loading-animation-id')
-const mainSection= document.getElementById('main-section-id')
+// const loadingScreen= document.getElementById('loading-screen-id')
+// const circle= document.getElementById('loading-animation-id')
+// const mainSection= document.getElementById('main-section-id')
 
-window.addEventListener("load",function(){
-    loadingScreen.style.transform="translateX(-500vh)";
-})
+// window.addEventListener("load",function(){
+//     loadingScreen.style.transform="translateX(-500vh)";
+// })
