158bedd600bdf84f3458dc99071cf5190ccc13ef
Jugal Hero <jugalnepali@gmail.com>
2023-01-08 14:57:48 +0545

    Version 2.0

--- Scripts/device-width.js
+++ Scripts/device-width.js
@@ -1,9 +1,11 @@
 const navBar= document.getElementById('nav-bar-id');
+const projectPhoto= document.querySelectorAll('#project-photo-id')
+const projectPara= document.querySelectorAll('#project-paragraph-id')
+const projectBioExpanded= document.querySelectorAll('#project-bio-expanded-id')
 var width= window.innerWidth;
 if(width<980)
 {
-    navBar.style.top="94%";
-    navBar.style.bottom="0%";
+    navBar.style.top="0%";
     navBar.style.left="0%";
     navBar.style.right="0%";
     navBar.style.padding="2vh";
@@ -11,6 +13,20 @@ if(width<980)
     navBar.style.alignItems="center";
     navBar.style.justifyContent="center";
     navBar.style.background="var(--s-color)";
-    navBar.style.boxShadow="0 0 1vh var(--shadow)";
+    navBar.style.boxShadow="0 0 2vh var(--shadow)";
+
+    projectPhoto.forEach(function(project){
+        project.style.width="100%";
+    })
+    projectPara.forEach(function(project){
+        project.style.width="100%";
+    })
+    projectBioExpanded.forEach(function(project){
+        project.style.width="90%";
+    })
     
+    // projectOverlay.style.top="0";
+    // projectOverlay.style.bottom="0";
+    // projectOverlay.style.right="0";
+    // projectOverlay.style.left="0";
 }
